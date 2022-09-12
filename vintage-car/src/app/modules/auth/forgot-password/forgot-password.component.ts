e3ef6d0439a244a63dcde92a05d26959f263aa67
assem <assem.hussein@mni.thm.de>
2022-09-12 13:01:55 +0200

    Auth Module fertig implementieren mit Views

--- vintage-car/src/app/modules/auth/forgot-password/forgot-password.component.ts
+++ vintage-car/src/app/modules/auth/forgot-password/forgot-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
@@ -7,10 +9,23 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent implements OnInit {
+  resetPasswordForm: FormGroup;
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService, private router: Router) {
+    this.resetPasswordForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  login() {
+    this.router.navigate(['auth/login']);
+  }
+
+  resetPassword() {
+    this.authService
+      .forgotPassword(this.resetPasswordForm.value.email);
+  }
 }
--- vintage-car/src/app/modules/auth/signup/signup.component.ts
+++ vintage-car/src/app/modules/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/services/auth.service';
 
@@ -8,18 +9,28 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+  signupForm: FormGroup;
 
   constructor(
     public authService: AuthService,
     private router: Router
-  ) { }
+  ) {
+    this.signupForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
 
   ngOnInit(): void {
   }
 
   login() {
     this.router.navigate(['auth/login']);
+  }
+
+  register() {
+    this.authService
+      .register(this.signupForm.value.email, this.signupForm.value.password)
   }
 
 }
