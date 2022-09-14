b6344989ef55805bb9a207ac244ded04bb298e4c
assem <assem.hussein@mni.thm.de>
2022-09-14 12:14:59 +0200

    Fahrzeug anzeigen

--- vintage-car/src/app/modules/fahrzeug/fahrzeug/fahrzeug.component.ts
+++ vintage-car/src/app/modules/fahrzeug/fahrzeug/fahrzeug.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, DocumentReference } from '@angular/fire/compat/firestore';
 import { Router } from '@angular/router';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { Lager } from 'src/app/shared/models/lager.model';
 import { Fahrzeug } from '../../../shared/models/fahrzeug.model';
 
 @Component({
@@ -14,6 +15,8 @@ export class FahrzeugComponent implements OnInit {
   fahrzeugSubject: BehaviorSubject<Fahrzeug> = new BehaviorSubject<Fahrzeug>(null);
   fahrzeug$: Observable<Fahrzeug> = this.fahrzeugSubject.asObservable();
   docRef: DocumentReference<unknown>;
+  lagerSubject: BehaviorSubject<Lager> = new BehaviorSubject<Lager>(null);
+  lager$: Observable<Lager> = this.lagerSubject.asObservable();
 
   constructor(private router: Router,
     private fireStore: AngularFirestore) {
@@ -21,10 +24,34 @@ export class FahrzeugComponent implements OnInit {
     const navigation = this.router.getCurrentNavigation();
     const state = navigation.extras?.state as { docRef: DocumentReference<unknown> };
     this.docRef = state?.docRef;
-    this.fahrzeug$ = fireStore.doc<Fahrzeug>(`fahrzeug/${this.docRef?.id}`).valueChanges();
+    const fahrzeugId = this.docRef ? this.docRef?.id : router.url.substring(10);
+
+    this.fahrzeug$ = fireStore.doc<Fahrzeug>(`fahrzeug/${fahrzeugId}`).valueChanges();
+
+    const lagerCollection = this.fireStore.collection<Lager>('lager');
+    const lager$ = lagerCollection.snapshotChanges();
+    lager$.subscribe((docActions) => {
+      let lager: any;
+      lager = docActions.map((docAction) => ({
+        id: docAction.payload.doc.id,
+        ...docAction.payload.doc.data(),
+      } as Lager));
+
+      lager = lager
+        .filter((lager: Lager) => lager
+          .stellplaetze?.some(stellplatz => stellplatz.fahrzeugId == fahrzeugId))[0];
+      this.lagerSubject.next(lager);
+    });
   }
 
   ngOnInit(): void {
   }
 
+  searchLager() {
+    throw new Error('Method not implemented.');
+  }
+
+  navLager(lagerId: string) {
+    this.router.navigate([`einrichtung/lager/${lagerId}`])
+  }
 }
